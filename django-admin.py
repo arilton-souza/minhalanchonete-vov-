497c6e3b61028495804dd580b5e542452e2d489f
arilton-souza <164657970+arilton-souza@users.noreply.github.com>
2024-10-15 15:54:14 -0300

    retorno

--- django-admin.py
+++ django-admin.py
@@ -1,3 +1,3 @@
-django-admin startproject lanchonete_vovo
-cd lanchonete_vovo
-python manage.py startapp produtos
+#django-admin startproject lanchonete_vovo
+#cd lanchonete_vovo
+#python manage.py startapp produtos
